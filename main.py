db65e3cfde95fcd16cc83fd4821218a86fe79866
nogaweisz <nogaweisz@gmail.com>
2025-01-08 21:00:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,4 +22,8 @@ model.fit(x_train, y_train)
 from sklearn.metrics import accuracy_score
 y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
+printfrom sklearn.metrics import accuracy_score
+
+y_pred = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
 print(accuracy)
